7b1d070e0fb6963b81da63083344d22c3404a877
muskan8299k@gmail.com <122015156+muskankumari8@users.noreply.github.com>
2024-08-23 18:46:15 +0530

    .

--- script.js
+++ script.js
@@ -190,26 +190,6 @@ async function main() {
     })
 
 
-    // // Add an event listener to previous
-    // previous.addEventListener("click", () => {
-    //     currentSong.pause()
-    //     console.log("Previous clicked")
-    //     let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
-    //     if ((index - 1) >= 0) {
-    //         playMusic(songs[index - 1])
-    //     }
-    // })
-
-    // // Add an event listener to next
-    // next.addEventListener("click", () => {
-    //     currentSong.pause()
-    //     console.log("Next clicked")
-
-    //     let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0])
-    //     if ((index + 1) < songs.length) {
-    //         playMusic(songs[index + 1])
-    //     }
-    // })
     
    
 // Add an event listener to previous
